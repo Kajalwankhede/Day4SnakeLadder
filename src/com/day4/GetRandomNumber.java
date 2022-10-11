3760d01b750812ae78229337467f3a7fbf1e2c94
Kajakwankhede <Kajalwankhede@example.com>
2022-10-11 19:37:07 +0530

    Snake Ladder program

---
+++ src/com/day4/GetRandomNumber.java
@@ -0,0 +1,16 @@
+package com.day4;
+import java.util.Random;
+public class GetRandomNumber {
+    GetRandomNumber()
+    {
+        System.out.println("****************Welcome To Snake And Ladder.*****************");
+    }
+    public static void main(String[] args) {
+        GetRandomNumber p=new GetRandomNumber();
+        int StartingPosition = 0;
+        System.out.println("Player Position :" +StartingPosition);
+        Random random= new Random();
+        int DieRoll=(random.nextInt(6)+1);
+        System.out.println("The Player rolls die & get number 1 to 6: "+DieRoll);
+    }
+}
--- src/com/day4/Uc1.java
+++
@@ -1,4 +0,0 @@
-package com.day4;
-
-public class Uc1 {
-}
