0f086bb17f80e92a81c2016d9ab139dd25efdac1
Kajakwankhede <Kajalwankhede@example.com>
2022-10-11 19:40:52 +0530

    To get Random Number from 1 to 6

--- src/com/day4/GetRandomNumber.java
+++ src/com/day4/GetRandomNumber.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class GetRandomNumber {
     GetRandomNumber()
     {
-        System.out.println("****************Welcome To Snake And Ladder.*****************");
+        System.out.println("***************Welcome To Snake And Ladder.*****************");
     }
     public static void main(String[] args) {
         GetRandomNumber p=new GetRandomNumber();
